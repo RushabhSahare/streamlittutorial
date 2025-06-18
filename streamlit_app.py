a1cc49b4b320b288e6aaf33fd5fafa0afc21c9c7
Rushabh Sahare <rushabhsahare81@hotmail.com>
2025-06-18 18:24:00 +0530

    Day 17 st.secrets added the functionality to NOT show the API, secrets

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ st.title('st.secrets')
 st.write(st.secrets["my_api"]["key"])
 st.write(st.secrets["my_api"]["base_url"])
 
-headers = {"Authorization": f"Bearer {api_key}"}
+headers = {"Authorization": f"Bearer {key}"}
 response = requests.get(base_url, headers=headers)
 
 st.write("API call made")
